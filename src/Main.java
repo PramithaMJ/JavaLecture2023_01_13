c1f2458992cbc0cd936ab1320a4eb07f0bbc9a4e
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-07-13 18:39:13 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       //character array to string
+        char[] ch ={'J','a','v','a',' ','S','t','r','i','n','g'};
+        String s =new String(ch);
+        System.out.println(s);
+
+        String str ="Java String";
+        System.out.println(str);
     }
 }
